765a4c6f3403881a11c1054f727fb12a3852201f
shivani <sks@lion.lmu.edu>
2024-03-10 21:45:00 -0700

    added the mentioned changes

--- nllb_serve/app.py
+++ nllb_serve/app.py
@@ -6,7 +6,7 @@ import os
 import sys
 import platform
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from sentence_splitter import split_text_into_sentences
+from .utils import sentence_splitter, ssplit_lang
 
 from functools import lru_cache
 import time
@@ -21,10 +21,6 @@ from . import log, DEF_MODEL_ID
 
 device = torch.device(torch.cuda.is_available() and 'cuda' or 'cpu')
 log.info(f'torch device={device}')
-
-dicti = {"cat_Latn": "ca", "ces_Latn": "cs", "dan_Latn": "da", "nld_Latn": "nl","eng_Latn": "en","fin_Latn": "fi","fra_Latn": "fr","deu_Latn": "de",
-"ell_Grek": "el","hun_Latn": "hu","isl_Latn": "is","ita_Latn": "it","Latvian": "lv","lit_Latn": "lt","nno_Latn": "no","pol_Latn": "pl",
-"por_Latn": "pt","ron_Latn": "ro","rus_Cyrl": "ru","slk_Latn": "sk","spa_Latn": "es","swe_Latn": "sv","tur_Latn": "tr"}
 
 #DEF_MODEL_ID = "facebook/nllb-200-distilled-600M"
 DEF_SRC_LNG = 'eng_Latn'
@@ -80,21 +76,6 @@ def jsonify(obj):
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(bp.root_path, 'static', 'favicon'), 'favicon.ico')
-
-
-def sentence_splitter(src_lang, sources): 
-    lang = dicti.get(src_lang)    
-    index = [0]
-    sentences = []
-    
-    for source in sources:
-        sentence = split_text_into_sentences(source, language=lang)
-        for sen in sentence:
-            sentences.append(sen)
-        index.append(len(sentences))
-
-    return sentences, index
-
 
 def attach_translate_route(
     model_id=DEF_MODEL_ID, def_src_lang=DEF_SRC_LNG,
@@ -152,10 +133,9 @@ def attach_translate_route(
             return "Please submit 'source' parameter", 400
         
         if sen_split:
-            if src_lang not in dicti:
-                return "Sentence splitter for this langauges is not availabe", 400
-            else:   
-                sources, index  = sentence_splitter(src_lang, sources)
+            if not ssplit_lang(src_lang):
+                return "Sentence splitter for this langauges is not availabe", 400 
+            sources, index  = sentence_splitter(src_lang, sources)           
         
         max_length = 80
         inputs = tokenizer(sources, return_tensors="pt", padding=True)
